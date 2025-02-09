173f6061b23aef9e7ff259a331704c3f400e25d9
Paul Jenkins <paul@turnpiece.com>
2025-02-09 23:48:05 +0000

    modified bootstrap for debugging

--- dmg-read-more/tests/bootstrap.php
+++ dmg-read-more/tests/bootstrap.php
@@ -1,42 +1,21 @@
 <?php
-/**
- * PHPUnit bootstrap file.
- *
- * @package Dmg_Read_More
- */
 
-echo "Bootstrap loaded";
+// Load the PHPUnit Polyfills.
+if ( file_exists( dirname( __DIR__ ) . '/vendor/autoload.php' ) ) {
+    require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+} elseif ( getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' ) ) {
+    require_once getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
+} else {
+    echo "PHPUnit Polyfills library not found. Please install it.\n";
+    exit( 1 );
+}
 
+// Continue with loading the WordPress test environment.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+    $_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
-echo $_tests_dir;
-
-// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
-$_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
-if ( false !== $_phpunit_polyfills_path ) {
-	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
-}
-
-if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
-	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
-
-// Give access to tests_add_filter() function.
 require_once "{$_tests_dir}/includes/functions.php";
-
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/dmg-read-more.php';
-}
-
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-// Start up the WP testing environment.
-require "{$_tests_dir}/includes/bootstrap.php";
+require_once "{$_tests_dir}/includes/bootstrap.php";
