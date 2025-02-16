aa6dc7699a95dbd89d73c21b7a2e02110b660012
Paul Jenkins <paul@turnpiece.com>
2025-02-16 17:38:20 +0000

    remove commented out polyfill code

--- dmg-read-more/tests/bootstrap.php
+++ dmg-read-more/tests/bootstrap.php
@@ -2,17 +2,7 @@
 
 // Load the PHPUnit Polyfills
 require_once dirname(__DIR__) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
-/*
-// Load the PHPUnit Polyfills.
-if ( file_exists( dirname( __DIR__ ) . '/vendor/autoload.php' ) ) {
-    require_once dirname( __DIR__ ) . '/vendor/autoload.php';
-} elseif ( getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' ) ) {
-    require_once getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
-} else {
-    echo "PHPUnit Polyfills library not found. Please install it.\n";
-    exit( 1 );
-}
-*/
+
 // Continue with loading the WordPress test environment.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
