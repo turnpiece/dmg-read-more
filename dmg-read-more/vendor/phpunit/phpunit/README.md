e6fa99496d97df96251bacf301b4ce8fd816768d
Paul Jenkins <paul@turnpiece.com>
2025-05-19 14:33:09 +0100

    Update composer.lock after version bump

--- dmg-read-more/vendor/phpunit/phpunit/README.md
+++ dmg-read-more/vendor/phpunit/phpunit/README.md
@@ -2,7 +2,7 @@
 
 [![Latest Stable Version](https://poser.pugx.org/phpunit/phpunit/v)](https://packagist.org/packages/phpunit/phpunit)
 [![CI Status](https://github.com/sebastianbergmann/phpunit/workflows/CI/badge.svg)](https://github.com/sebastianbergmann/phpunit/actions)
-[![codecov](https://codecov.io/gh/sebastianbergmann/phpunit/branch/8.5/graph/badge.svg)](https://codecov.io/gh/sebastianbergmann/phpunit)
+[![codecov](https://codecov.io/gh/sebastianbergmann/phpunit/branch/9.6/graph/badge.svg)](https://codecov.io/gh/sebastianbergmann/phpunit)
 
 PHPUnit is a programmer-oriented testing framework for PHP. It is an instance of the xUnit architecture for unit testing frameworks.
 
--- dmg-read-more/vendor/phpunit/phpunit/src/Util/Log/TeamCity.php
+++ dmg-read-more/vendor/phpunit/phpunit/src/Util/Log/TeamCity.php
@@ -32,19 +32,18 @@ use PHPUnit\Framework\TestFailure;
 use PHPUnit\Framework\TestResult;
 use PHPUnit\Framework\TestSuite;
 use PHPUnit\Framework\Warning;
-use PHPUnit\TextUI\ResultPrinter;
+use PHPUnit\TextUI\DefaultResultPrinter;
 use PHPUnit\Util\Exception;
 use PHPUnit\Util\Filter;
 use ReflectionClass;
 use ReflectionException;
 use SebastianBergmann\Comparator\ComparisonFailure;
-use SebastianBergmann\Timer\RuntimeException;
 use Throwable;
 
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
-final class TeamCity extends ResultPrinter
+final class TeamCity extends DefaultResultPrinter
 {
     /**
      * @var bool
@@ -61,9 +60,6 @@ final class TeamCity extends ResultPrinter
      */
     private $flowId;
 
-    /**
-     * @throws RuntimeException
-     */
     public function printResult(TestResult $result): void
     {
         $this->printHeader($result);
@@ -82,7 +78,7 @@ final class TeamCity extends ResultPrinter
                 'message'  => self::getMessage($t),
                 'details'  => self::getDetails($t),
                 'duration' => self::toMilliseconds($time),
-            ]
+            ],
         );
     }
 
@@ -165,7 +161,7 @@ final class TeamCity extends ResultPrinter
         }
     }
 
-    public function printIgnoredTest($testName, Throwable $t, float $time): void
+    public function printIgnoredTest(string $testName, Throwable $t, float $time): void
     {
         $this->printEvent(
             'testIgnored',
@@ -174,7 +170,7 @@ final class TeamCity extends ResultPrinter
                 'message'  => self::getMessage($t),
                 'details'  => self::getDetails($t),
                 'duration' => self::toMilliseconds($time),
-            ]
+            ],
         );
     }
 
@@ -194,7 +190,7 @@ final class TeamCity extends ResultPrinter
 
             $this->printEvent(
                 'testCount',
-                ['count' => count($suite)]
+                ['count' => count($suite)],
             );
         }
 
@@ -276,7 +272,7 @@ final class TeamCity extends ResultPrinter
             [
                 'name'     => $test->getName(),
                 'duration' => self::toMilliseconds($time),
-            ]
+            ],
         );
     }
 
@@ -356,7 +352,7 @@ final class TeamCity extends ResultPrinter
         return str_replace(
             ['|', "'", "\n", "\r", ']', '['],
             ['||', "|'", '|n', '|r', '|]', '|['],
-            $text
+            $text,
         );
     }
 
@@ -372,7 +368,7 @@ final class TeamCity extends ResultPrinter
             throw new Exception(
                 $e->getMessage(),
                 $e->getCode(),
-                $e
+                $e,
             );
         }
         // @codeCoverageIgnoreEnd
